bdbf88427e3ff63d9e6894ef084b1a14dc9dfcf3
Abu Foysal <abu.foysal07@gmail.com>
2017-09-24 23:21:20 +0600

    Server changed to Al Adhan

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # prayer_time
-## Uses data from Muslimsalat.com
-Demo: curl -4 hash404.com/prayer/   
+## Uses data from AlAdhan.com
+Demo: curl -4 hash404.com/prayer/
 Uses: ipify.org to fetch IP address on local machine.
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 $ip = getenv('REMOTE_ADDR'); //remote ip address
 $local = false;
 if ($local){
@@ -12,18 +13,17 @@ if ($local){
 }
 $query = @unserialize(file_get_contents('http://ip-api.com/php/'.$ip));
 if($query && $query['status'] == 'success') {
-  echo 'Hello visitor from '.$query['country'].', '.$query['city'].'!';
+  echo 'Hello visitor from ' . $query['country'] . '(' . $query['countryCode'] . '), '. $query['city'] .'!';
 } else {
   die('Unable to get location');
 }
 $city = preg_replace('/\s+/', '', $query['city']); //remove all white spaces
-$url='http://muslimsalat.com/'.$city.'.json'; //query api site for json
+$url="http://api.aladhan.com/timingsByCity?city=$city&country=" . $query['countryCode'] . "&method=2"; //query api site for json
+
 $data= file_get_contents($url);
 $obj = json_decode($data);
-echo "\nFajr: ".$obj->items[0]->fajr;
-echo "\nShurooq: ".$obj->items[0]->shurooq;
-echo "\nDhuhr: ".$obj->items[0]->dhuhr;
-echo "\nAsr: ".$obj->items[0]->asr;
-echo "\nMaghrib: ".$obj->items[0]->maghrib;
-echo "\nIsha: ".$obj->items[0]->isha."\n";
+foreach ($obj->data->timings as $key => $value) {
+  echo "\n$key  $value\n";
+}
+
 ?>
